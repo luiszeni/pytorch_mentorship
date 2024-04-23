16d7855dfe528a7c3a0667115c5a7e2f6ea7605d
luis zeni <luisfelipezeni@gmail.com>
2024-04-23 10:45:26 -0300

    Implementação correta do dataset e implementação do modelo de classificação.

--- class_04_dataset_and_dataloader/heart_desease_dataset.py
+++ class_04_dataset_and_dataloader/heart_desease_dataset.py
@@ -1,17 +1,39 @@
 
+import os
+import pandas as pd
+import torch
 from torch.utils.data.dataset import Dataset
+
 
 class HeartDeseaseDataset(Dataset):
 
-    def __init__(self):
+    def __init__(self, dataset_location, train=True):
         super().__init__()
 
-        self.x = [0, 1, 4, 6, 7, 7 ,5, 3]
-        self.y = [0, 0, 1, 1, 0, 0 ,1, 0]
+        csv_path = 'train.csv' if train else 'test.csv'
+        csv_path = os.path.join(dataset_location, csv_path)
+
+        self.dataset_df = pd.read_csv(csv_path)
+
+        self.n_of_features = len(self.dataset_df.columns) - 2
+        self.n_of_classes  = int(self.dataset_df['target'].max() + 1)
+   
     
     def __len__(self):
-        return len(self.x)
+        return len(self.dataset_df)
     
-    def __getitem__(self, idx):
 
-        return self.x[idx], self.y[idx]
+    def __getitem__(self, idx):
+        data_sample = self.dataset_df.iloc[idx]
+
+        x_cols = list(data_sample.keys())
+        del x_cols[0]
+        del x_cols[x_cols.index('target')]
+
+        x = data_sample[x_cols]
+        y = data_sample['target']
+
+        x = torch.tensor(x, dtype=torch.float)
+        y = torch.tensor(y, dtype=torch.long)
+        
+        return x, y
---
+++ class_05_my_first_nn/mp_model.py
@@ -0,0 +1,23 @@
+from torch import nn
+
+
+class MPModel(nn.Module):
+
+    def __init__(self, in_size=30, n_classes=2):
+        super(MPModel, self).__init__()
+
+        self.cls_head = nn.Sequential(
+            nn.Linear(in_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, n_classes),
+        )
+
+    def forward(self, x):
+        return self.cls_head(x)
+
+
